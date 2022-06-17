dee73a3f70ed0a6ac643507f07574d332908231d
Kevin Bueno <kean.solano@gmail.com>
2022-06-17 17:22:08 -0500

    get and post methods

--- api/api.go
+++ api/api.go
@@ -5,14 +5,21 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/gorilla/schema"
 )
 
 type API struct {
 }
 
+// schema convert the params to the data type
 type BooksParams struct {
-	Limit int `schema:"limit`
+	Offset int `schema:"offset`
+	Limit  int `schema:"limit`
+}
+
+type PostBook struct {
+	Title string `json:"offset`
 }
 
 var (
@@ -24,20 +31,66 @@ func (a *API) getBooks(w http.ResponseWriter, r *http.Request) {
 
 	params := &BooksParams{}
 
-	limitParam := r.URL.Query().Get("limit")
-	limit, err := strconv.Atoi(limitParam)
+	err := decoder.Decode(params, r.URL.Query())
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	if limit < 0 || limit > len(books) {
+
+	if params.Offset > len(books) || params.Offset < 0 {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	json.NewEncoder(w).Encode(books[:limit])
+	if params.Limit < 0 || params.Limit > len(books) {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var from, to int
+	if params.Offset > 0 {
+		from = params.Offset
+	}
+
+	if params.Limit > 0 {
+		to = params.Limit
+	} else {
+		to = len(books)
+	}
+
+	json.NewEncoder(w).Encode(books[from:to])
 }
 
 func (a *API) getBook(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	idParam := pathParams["id"]
 
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	idx := id - 1
+
+	if idx < 0 || idx > len(books)-1 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(books[idx])
+
+}
+
+func (a *API) postBook(w http.ResponseWriter, r *http.Request) {
+	book := &PostBook{}
+
+	err := json.NewDecoder(r.Body).Decode(book)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	books = append(books, book.Title)
+	w.WriteHeader(http.StatusCreated)
 }
--- api/routes.go
+++ api/routes.go
@@ -8,5 +8,6 @@ import (
 
 func (a *API) RegisterRoutes(r *mux.Router) {
 	r.HandleFunc("/books", a.getBooks).Methods(http.MethodGet)
+	r.HandleFunc("/books", a.postBook).Methods(http.MethodPost)
 	r.HandleFunc("/books/{id}", a.getBook).Methods(http.MethodGet)
 }
