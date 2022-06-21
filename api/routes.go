5802fd5db00f034a8402b0fe3a422830fc99470d
Kevin Bueno <kean.solano@gmail.com>
2022-06-20 20:59:34 -0500

    adding middlewares

--- api/routes.go
+++ api/routes.go
@@ -7,7 +7,16 @@ import (
 )
 
 func (a *API) RegisterRoutes(r *mux.Router) {
-	r.HandleFunc("/books", a.getBooks).Methods(http.MethodGet)
-	r.HandleFunc("/books", a.postBook).Methods(http.MethodPost)
-	r.HandleFunc("/books/{id}", a.getBook).Methods(http.MethodGet)
+	r.Use(requestIDhandler)
+	
+	public := r.NewRoute().Subrouter()
+	private := r.NewRoute().Subrouter()
+	
+	private.Use(authMiddleware)
+
+	public.HandleFunc("/books", a.getBooks).Methods(http.MethodGet)
+	public.HandleFunc("/books/{id}", a.getBook).Methods(http.MethodGet)
+
+	private.HandleFunc("/books", a.postBook).Methods(http.MethodPost)
+
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,6 @@ go 1.18
 
 require github.com/gorilla/mux v1.8.0
 
-require github.com/gorilla/schema v1.2.0 // indirect
+require github.com/gorilla/schema v1.2.0
+
+require github.com/google/uuid v1.3.0
